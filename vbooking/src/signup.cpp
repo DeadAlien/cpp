4fae50ec1dbb3e45bf40db10429b96a060baf41c
ALIEN <devashishranjan7@gmail.com>
2025-05-18 08:58:08 +0000

    adding password check

--- vbooking/src/signup.cpp
+++ vbooking/src/signup.cpp
@@ -4,40 +4,6 @@ SignUp::SignUp(){
     //std::cout << "Signup Object is created" << std::endl;
 }
 
-void SignUp::RegisterNewAccount(void){
-/* Using this fucntion user can create there account and the given info will save in 
-information file (Information file is creating in this function).
-*/
-    signup b;
-    std::cout << "Register New Account" << std::endl;
-
-    // std::cout << "Name: " << endl;
-    // std::cin >> b.UserName;
-    std::cout << "Email: " << endl;
-    if(b.EmailVerification() != true)
-    {
-        std::cout << "Please enter valid email." << std::endl; 
-    }
-    // std::cout << "Password: " << endl;
-    // std::cin >> b.Password;
-
-    std::ofstream fopen("information");
-
-    if(!fopen)
-    {
-        std::cout << "Error:Information: Could not open file for writing." << std::endl;
-        return;
-    }
-    else
-    {
-        /* follow standard template.*/
-    }
-
-    fopen << b.UserName << "--" << b.Email << "--" << b.Password << std::endl;
-    fopen.close();
-    std::cout << "Info saved!!" << std::endl;
-}
-
 bool SignUp::EmailVerification(){
 /*Email verification code whicle can verify email syntex like name@domain.com
 some domain like gmail.com, yahoo.com, yahoo.in, hotmain.in, msn.com
@@ -83,6 +49,92 @@ some domain like gmail.com, yahoo.com, yahoo.in, hotmain.in, msn.com
     }
     return false;
 }
+
+
+bool SignUp::PasswordCheck(){
+    /* 
+        use (A-Z), (a-z), (0-9), (@,#,$,%,*) 
+        spcae should not be there    
+    */
+
+    signup p;
+    cin >> p.Password;
+    string pas = p.Password;
+    int len = pas.length();
+    bool capFlag = false;
+    bool smallFlag = false;
+    bool SpecialCharFlag = false;
+
+    if(len < 12)
+    {
+        return false;
+    }
+
+    for(int i = 0; i < len; i++)
+    {
+        if((pas[i] >= 'A') && (pas[i] <= 'Z'))
+        {
+            capFlag = true;
+        }
+        if((pas[i] >= 'a') && (pas[i] <= 'z'))
+        {
+            smallFlag = true;
+        }
+        if((pas[i] == '@') || (pas[i] == '#') 
+            || (pas[i] == '$') || (pas[i] == '%') 
+            || (pas[i] == '*'))
+        {
+            SpecialCharFlag = true;
+        }
+    }
+
+    if((capFlag == true) && (smallFlag == true) && (SpecialCharFlag == true))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+void SignUp::RegisterNewAccount(void){
+/* Using this fucntion user can create there account and the given info will save in 
+information file (Information file is creating in this function).
+*/
+    signup b;
+    std::cout << "Register New Account" << std::endl;
+
+    std::cout << "Name: " << endl;
+    std::cin >> b.UserName;
+    std::cout << "Email: " << endl;
+    if(b.EmailVerification() != true)
+    {
+        std::cout << "Please enter valid email." << std::endl; 
+        return;
+    }
+    std::cout << "Password: " << endl;
+    if(b.PasswordCheck() != true)
+    {
+        std::cout << "Pasword should contain: Atlist 12 char (A-Z), (a-z), (0-9), (@,#,$,%,*)" << std::endl;
+        return;
+    }
+
+    std::ofstream fopen("information");
+
+    if(!fopen)
+    {
+        std::cout << "Error:Information: Could not open file for writing." << std::endl;
+        return;
+    }
+    else
+    {
+        /* follow standard template.*/
+    }
+
+    fopen << b.UserName << "--" << b.Email << "--" << b.Password << std::endl;
+    fopen.close();
+    std::cout << "Info saved!!" << std::endl;
+}
+
 
 SignUp::~SignUp(){
    // std::cout << "Signup Object is destroyed" << std::endl;
